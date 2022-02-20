8165d679c9bddab814c9522a330fc7ebbf06bca5
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:42:18 -0800

    Update Last_Cookie.py

--- Last_Cookie.py
+++ Last_Cookie.py
@@ -5,26 +5,20 @@ cookies = 16
 player1_turn = True
 
 while cookies > 0:
-
     print("There are " + str(cookies) + " cookies left in the jar ")
 
     if player1_turn:
         current_player = "Player 1"
 
     else:
-
         current_player = "player 2"
 
     print(current_player + ", it's your turn. ")
-
     take_out_string = input("How many cookies do you want to take out? ")
-
     take_out = int(take_out_string)
-
     cookies -= take_out
 
     print("")
-
     player1_turn = not player1_turn
 
 print("Congratulations, " + current_player + " you are the winner!")
